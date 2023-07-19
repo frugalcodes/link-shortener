50a28d76cd99e10dd2f11eb9756bdb60c9025247
Samuel Igwe <samueligwe63@gmail.com>
2023-07-19 12:54:47 +0100

    Updates

---
+++ precache-manifest.2a259e1fd6791883615639c8a0983193.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "153c41fdcc3c6e13d931",
+    "url": "/link-shortener/static/css/main.538a3454.chunk.css"
+  },
+  {
+    "revision": "153c41fdcc3c6e13d931",
+    "url": "/link-shortener/static/js/main.153c41fd.chunk.js"
+  },
+  {
+    "revision": "2745ac762d35ee0e1f8a",
+    "url": "/link-shortener/static/js/1.2745ac76.chunk.js"
+  },
+  {
+    "revision": "4b4f4a607511eba0f098",
+    "url": "/link-shortener/static/js/runtime~main.4b4f4a60.js"
+  },
+  {
+    "revision": "4285c880c61edcaf6748698957b5e8d7",
+    "url": "/link-shortener/index.html"
+  }
+];
